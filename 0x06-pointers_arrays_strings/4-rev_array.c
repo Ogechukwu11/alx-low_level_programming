c59bb0dc9d32c6030b2904bba932d3bbb78f4f73
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-07-27 15:46:11 -0700

    I am a kind of paranoid in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverses the content of an array of integers
+ * @a: pointer of the array
+ * @n: number of elements of the array
+ * Return: 0 (success)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}

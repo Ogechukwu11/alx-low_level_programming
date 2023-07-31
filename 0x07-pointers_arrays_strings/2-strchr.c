deaa4516704630c7acb74ff3001216b62d989622
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-07-31 06:38:11 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,10 @@ char *_strchr(char *s, char c)
 {
 	int j;
 
-	for (j = 0; s[j] != '\0'; j++)
+	for (j = 0; s[j] >= '\0'; j++)
 	{
 		if (s[j] == c)
 			return (&s[j]);
 	}
-	return (NULL);
+	return (0);
 }

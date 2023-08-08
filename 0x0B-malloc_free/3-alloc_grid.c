991a01da1304cc8be705213c336f7f98b5ee2884
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-08 00:13:29 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Entry point
+ * @width: first array
+ * @height: second array
+ * Return: a pointer to the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}

b1c09644f4e9171e8d01c6a83510da4c72ee7a18
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-17 20:44:34 -0700

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (count = 0; count < n; count++)
 	{
-		str = va_arg(mystr, const char *);
+		str = va_arg(mystr, char *);
 
 		if (str == NULL)
 			printf("nil");

f6848d39893d048e83770fb19b96bcdf0d1724d2
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-08-25 06:39:05 -0700

    The Hare and the Tortoise #advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * main_100 - To print a string.
+ *
+ * Return: void
+ */
+
+void main_100(void)
+{
+	char *t;
+
+	t = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", t);
+}

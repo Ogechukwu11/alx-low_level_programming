cae891fd999c5a97d6a97bfb0c50d6e40bee3623
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-09-04 23:42:54 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * read_textfile - To read a text file and print it to the POSIX standard output.
+ * read_textfile - Read a text file and print it to the POSIX standard output.
  * @filename: A pointer to the file.
- * @letter: The letters.
+ * @letters: The number of letters it should read and print
  * Return: Returns the actual number of letters it could read and print.
  */
 
@@ -25,18 +25,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	read_byte = read(ptr, bf, letters);
-	close(ptr);
 	if (read_byte == -1)
 	{
-		close(ptr);;
-		free(bf);
+		close(ptr);
 		return (0);
 	}
 	write_byte = write(STDOUT_FILENO, bf, read_byte);
-	free(bf);
 	if (write_byte != read_byte)
 	{
 		return (0);
 	}
+	close(ptr);
+	free(bf);
 	return (write_byte);
 }

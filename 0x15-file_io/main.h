0e300ec1f3b07752a4bf5740a91c7ce9bd16f2b3
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-09-06 08:27:34 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int _str(char *s);
-
+char *_buffer(char *c);
+void c_file(int f);
+int main(int argc, char *argv[]);
 #endif

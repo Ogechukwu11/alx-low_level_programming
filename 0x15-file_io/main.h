d522ee2a7ffec15d8881dc5d6a793b1080f5233e
Ogechukwu11 <nwobumogechukwu@gmail.com>
2023-09-23 15:29:37 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <elf.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -15,4 +17,14 @@ int _str(char *s);
 char *_buffer(char *c);
 void c_file(int f);
 int main(int argc, char *argv[]);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 #endif

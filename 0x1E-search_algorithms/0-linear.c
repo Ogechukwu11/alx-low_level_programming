c618658316447a3423c38581dd3809648cccbb4a
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-03-12 07:02:25 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,13 +12,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL || value == 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		if (array[i] == value)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
